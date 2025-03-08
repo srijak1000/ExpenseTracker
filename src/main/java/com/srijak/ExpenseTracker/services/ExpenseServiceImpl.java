95f96ccf6644781660d01467c570b12b921afeda
Srijak Chakraborty <srijak.work@gmail.com>
2025-03-09 02:24:16 +0530

    beautified
    Signed-off-by: Srijak Chakraborty <srijak.work@gmail.com>

--- src/main/java/com/srijak/ExpenseTracker/services/ExpenseServiceImpl.java
+++ src/main/java/com/srijak/ExpenseTracker/services/ExpenseServiceImpl.java
@@ -11,11 +11,9 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ExpenseServiceImpl implements ExpenseService {
+
     private final ExpenseRepository expenseRepository;
 
-    public Expense postExpense(ExpenseDTO expenseDTO){
-        return saveOrUpdateExpense(new Expense(), expenseDTO);
-    }
     private Expense saveOrUpdateExpense(Expense expense, ExpenseDTO expenseDTO){
         expense.setTitle(expenseDTO.getTitle());
         expense.setDate(expenseDTO.getDate());
@@ -24,6 +22,10 @@ public class ExpenseServiceImpl implements ExpenseService {
         expense.setDescription((expenseDTO.getDescription()));
 
         return expenseRepository.save(expense);
+    }
+
+    public Expense postExpense(ExpenseDTO expenseDTO){
+        return saveOrUpdateExpense(new Expense(), expenseDTO);
     }
 
     public List<Expense> getAllExpenses(){
